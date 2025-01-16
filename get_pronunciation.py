e49f836336ea5c04f6009b34480954da42adfc2e
jwyxym <jwyxym@126.com>
2025-01-16 17:18:54 +0800

    Update get_pronunciation.py

--- get_pronunciation.py
+++ get_pronunciation.py
@@ -6,26 +6,41 @@ def is_polyphone(text, chk):
         return text
     t = ''
     for v, char in enumerate(text):
-        c = parse.quote(char)
-        url = f'https://hanyu.baidu.com/zici/s?from=aladdin&query={c}&smp_names=&smpid=&srcid=51368&wd={c}'
-        html = get_html(url)
-        b_list = find_pinyin(html, 'pinyin')
-        if len(b_list) > 1:
+        list_ = find_pinyin_by_char(char)
+        if len(list_) > 1:
             a = -1
-            while a >= len(b_list) or a < 0:
+            while a >= len(list_) or a < 0:
                 p = f'第{v + 1}个字符“{char}”是多音字，请选择读音：\n'
-                for i, b in enumerate(b_list):
+                for i, b in enumerate(list_):
                     p += f'{i + 1}. {b}\n'
                 a = input(p)
                 while not a.isdigit():
                     a = input('请输入正确的数字：')
                 a = int(a)
                 a -= 1
-            t += b_list[a]
-            t += ' '
+            t += only_four_tones(list_[a], char)
         else:
             t += char
     return t
+
+def find_pinyin_by_char(char):
+    c = parse.quote(char)
+    list_ = []
+    step = 0
+    while len(list_) == 0:
+        match step:
+            case 0:
+                url = f'https://hanyu.baidu.com/zici/s?from=aladdin&query={c}&smp_names=&smpid=&srcid=51368&wd={c}'
+                html = get_html(url)
+                list_ = find_pinyin_by_hanyu(html, 'pinyin')
+            case 1:
+                url = f'https://baike.baidu.com/item/{c}'
+                html = get_html(url)
+                list_ = find_pinyin_by_baike(html)
+            case 2:
+                break
+        step += 1
+    return list_
 
 def get_html(url):
     try:
@@ -35,12 +50,32 @@ def get_html(url):
     except Exception as e:
         return None
 
-def find_pinyin(html, element_id):
-    b_list = []
+def find_pinyin_by_baike(html):
+    html = str(html)
+    list_ = []
+    text = html[html.find('（拼音：') :]
+    text = text[: text.find('）')]
+    t = ''
+    for i in text:
+        if i.islower():
+            t += i
+        elif len(t) > 0:
+            list_.append(t)
+            t = ''
+    return list_
+
+def find_pinyin_by_hanyu(html, element_id):
+    list_ = []
     soup = BeautifulSoup(html, 'html.parser')
     pinyin_div = soup.find(id = element_id)
     if pinyin_div:
         b_tags = pinyin_div.find_all('b')
         for b_tag in b_tags:
-            b_list.append(b_tag.get_text())
-    return b_list
+            list_.append(b_tag.get_text())
+    return list_
+
+def only_four_tones(pinyin, char):
+    for c in pinyin:
+        if not c in 'abcdefghijklmnopqrstuvwxyz':
+            return f'{pinyin} '
+    return char
